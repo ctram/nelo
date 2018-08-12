c92057e98deb04a25fbba170cd316261e61b7c6f
Chris Tram <chris.tram@gmail.com>
2018-08-11 18:40:10 -0700

    override devise's default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
-  devise_for :users
-
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   root 'home#index'
+
+  # get '/users/sign-up', to: ''
 end
