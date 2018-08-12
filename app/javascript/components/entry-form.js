57c18b7db805c157d5fd31f3c60ea058ac489801
Chris Tram <chris.tram@gmail.com>
2018-08-12 02:37:11 -0700

    adds entry form component

--- app/javascript/components/entry-form.js
+++ app/javascript/components/entry-form.js
@@ -3,30 +3,35 @@ import React from 'react';
 export default class EntryForm extends React.Component {
   constructor(props) {
     super(props);
-    const { entryContent, entryTitle } = props;
+    const { entryContent, entryTitle, id } = props;
     this.state = {
       entryTitle,
-      entryContent
+      entryContent,
+      heading: id ? 'Edit Entry' : 'Create Entry'
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange() {}
-
-  handleSubmit() {}
-
   render() {
-    const { entryContent, entryTitle } = this.state;
+    const { heading } = this.state;
+    const authenticityToken = document.getElementsByTagName('meta')[1].getAttribute('content');
 
     return (
-      <div>
-        <input value={entryTitle} onChange={this.handleChange} />
-        <textarea value={entryContent} onChange={this.handleChange} />
-        <button role="button" onClick={this.handleSubmit}>
-          Save
-        </button>
+      <div className="row justify-content-center entry-form">
+        <div className="col-6">
+          <h2>{heading}</h2>
+          <form action="/entries" method="POST" acceptCharset="UTF-8">
+            <input name="utf8" type="hidden" value="✓" />
+            <input type="hidden" name="authenticity_token" value={authenticityToken} />
+            <div className="form-group">
+              <label htmlFor="title-input">Title</label>
+              <input id="title-input" className="form-control" name="entry[title]" />
+            </div>
+            <div className="form-group">
+              <textarea className="form-control" name="entry[content]" />
+            </div>
+            <button className="btn btn-primary">Save</button>
+          </form>
+        </div>
       </div>
     );
   }
