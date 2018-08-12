94f661d0c12ae4b884855c537f0c80f03ff203dc
Chris Tram <chris.tram@gmail.com>
2018-08-12 02:37:20 -0700

    adds entry component

--- app/javascript/components/entry.js
+++ app/javascript/components/entry.js
@@ -6,10 +6,12 @@ export default class Entry extends React.Component {
   }
 
   render() {
-    const { entryContent, entryTitle } = this.props;
+    const {
+      entry: { title: entryTitle, content: entryContent }
+    } = this.props;
 
     return (
-      <div>
+      <div className="entry">
         <h3>{entryTitle}</h3>
         <div>{entryContent}</div>
       </div>
