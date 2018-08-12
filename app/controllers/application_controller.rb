650b720dfba4e77dd70936bd8690325cf5be947e
Chris Tram <chris.tram@gmail.com>
2018-08-11 23:03:51 -0700

    set current_user to props

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  around_action :set_current_user_to_props
+
+  private
+
+  def set_current_user_to_props
+    @props = { currentUser: current_user }
+    yield
+  end
+  
 end
