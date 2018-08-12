2e557ac69f8910247b74e8719700f8270e31946b
Chris Tram <chris.tram@gmail.com>
2018-08-12 02:36:08 -0700

    adds create action for entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,23 @@
 class EntriesController < ApplicationController
   def index
+    puts "index called"
     @entries = Entry.where(user_id: current_user.id)
     @react_bundle = 'entries-bundle'
   end
   
   def new
+    puts "new called"
     @entry = Entry.new(user_id: current_user.id)
     @react_bundle = 'entry-form-bundle'
   end
   
   def show
     @entry = Entry.find(params[:id])
-    @entry
+  end
+
+  def create
+    current_user.entries.create(entry_params)
+    redirect_to entries_path
   end
 
   private
