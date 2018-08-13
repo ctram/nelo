32fc79b563a15f88374ac45dff4e7e5169ddc18c
Chris Tram <chris.tram@gmail.com>
2018-08-13 15:06:29 -0700

    remove before filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_default_react_bundle
   before_action :redirect_if_not_logged_in
 
   private
 
   def redirect_if_not_logged_in
     redirect_to '/users/sign_in' unless signed_in?
-  end
-  
-  def set_default_react_bundle
-    @react_bundle = 'navbar-bundle'
   end
 end
