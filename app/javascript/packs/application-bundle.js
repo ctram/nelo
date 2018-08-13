390d577c6781c7f3fd96e68f66ae6b46668c3e0d
Chris Tram <chris.tram@gmail.com>
2018-08-13 15:11:16 -0700

    use master bundle

--- app/javascript/packs/application-bundle.js
+++ app/javascript/packs/application-bundle.js
@@ -1,8 +1,14 @@
 import EntryForm from '../components/entry-form';
+import Entries from '../components/entries';
+import Landing from '../components/landing';
+import Modal from '../components/modal';
 import Navbar from '../components/navbar';
 import ReactOnRails from 'react-on-rails';
 
 ReactOnRails.register({
   Navbar,
-  EntryForm
+  Entries,
+  EntryForm,
+  Landing,
+  Modal
 });
--- app/javascript/packs/entries-bundle.js
+++
@@ -1,9 +0,0 @@
-import Entries from '../components/entries';
-import Navbar from '../components/navbar';
-import ReactOnRails from 'react-on-rails';
-
-ReactOnRails.register({
-  Navbar,
-  Entries
-});
-
