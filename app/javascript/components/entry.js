076e9f8732dc0f89631946095aedc4f2e7eee711
Chris Tram <chris.tram@gmail.com>
2018-08-13 15:06:57 -0700

    adds edit and delete buttons to entry

--- app/javascript/components/entry.js
+++ app/javascript/components/entry.js
@@ -11,9 +11,23 @@ export default class Entry extends React.Component {
     } = this.props;
 
     return (
-      <div className="entry">
-        <h3>{entryTitle}</h3>
-        <div>{entryContent}</div>
+      <div className="entry d-flex justify-content-between">
+        <div className="entry__details">
+          <h3>{entryTitle}</h3>
+          <div>{entryContent}</div>
+        </div>
+        <div className="entry__actions">
+          <button
+            role="button"
+            className="btn btn-outline-primary btn-small mr-3"
+            onClick={this.edit}
+          >
+            Edit
+          </button>
+          <button role="button" className="btn btn-outline-danger btn-small" onClick={this.delete}>
+            Delete
+          </button>
+        </div>
       </div>
     );
   }
