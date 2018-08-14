0e30bc5da3f371d7e61ffae3da346cbebeec8944
Chris Tram <chris.tram@gmail.com>
2018-08-14 12:18:33 -0700

    stub out pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    length = Entry.all.length
+    start_idx = length - 10 > 0 ? length - 10 : 0
+    @entries = Entry.all.slice(start_idx, length)
   end
 end
