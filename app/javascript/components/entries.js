d24ae27f7e071dd52309e2233aa07bca4ff87ffd
Chris Tram <chris.tram@gmail.com>
2018-08-14 01:21:11 -0700

    refactor entry delete event from entries

--- app/javascript/components/entries.js
+++ app/javascript/components/entries.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import Entry from './entry';
-import Modal from './modal';
 import CONSTANTS from '../constants';
+import ModalConfirmDeleteEntry from './modals/modal-confirm-delete-entry';
+import entryActions from '../actions/entry';
 
 export default class Entries extends React.Component {
   constructor(props) {
     super(props);
     this.delete = this.delete.bind(this);
     this.cancelDelete = this.cancelDelete.bind(this);
-    this.areYouSureDelete = this.areYouSureDelete.bind(this);
+    this.modalConfirmDeleteEntry = this.modalConfirmDeleteEntry.bind(this);
 
     this.state = {
-      modalAreYouSureVisible: false
+      modalConfirmDeleteVisible: false
     };
   }
 
   delete(id) {
-    const authenticityToken = document.getElementsByTagName('meta')[1].getAttribute('content');
-
-    const fd = new FormData();
-    fd.set('authenticity_token', authenticityToken);
-
-    fetch(CONSTANTS.appDomainURL + '/entries' + `/${id}`, { method: 'DELETE', body: fd }).then(
-      () => {
-        window.location.href = CONSTANTS.appDomainURL;
-      }
-    );
+    entryActions.deleteEntry(id);
   }
 
   edit(id) {
@@ -33,44 +25,23 @@ export default class Entries extends React.Component {
   }
 
   cancelDelete() {
-    this.setState({ modalAreYouSureVisible: false });
+    this.setState({ modalConfirmDeleteVisible: false });
   }
 
-  areYouSureDelete(id) {
-    const modalDetails = {
-      cssID: 'areYouSure',
-      title: 'Are You Sure?',
-      content: 'This cannot be undone.',
-      footer: (
-        <div>
-          <button
-            className="btn btn-danger"
-            onClick={() => {
-              this.delete(id);
-            }}
-          >
-            Delete
-          </button>
-          <button className="btn btn-info" onClick={this.cancelDelete}>
-            Cancel
-          </button>
-        </div>
-      )
-    };
-
-    this.setState({ modalAreYouSureVisible: true, modalDetails });
+  modalConfirmDeleteEntry(id) {
+    this.setState({ modalConfirmDeleteVisible: true, itemToDeleteID: id });
   }
 
   render() {
     let { entries } = this.props;
-    const { modalDetails, modalAreYouSureVisible } = this.state;
+    const { modalConfirmDeleteVisible, itemToDeleteID } = this.state;
     let entriesDOM = <div className="text-center">No entries</div>;
 
     if (entries.length > 0) {
       entriesDOM = entries.map((entry, idx) => {
         return (
           <div key={entry.id}>
-            <Entry entry={entry} onDelete={this.areYouSureDelete} />
+            <Entry entry={entry} onDelete={this.modalConfirmDeleteEntry} />
             {idx !== entries.length - 1 && <hr />}
           </div>
         );
@@ -79,7 +50,12 @@ export default class Entries extends React.Component {
 
     return (
       <div className="row justify-content-center">
-        <Modal cssID="modal-are-you-sure" details={modalDetails} visible={modalAreYouSureVisible} />
+        <ModalConfirmDeleteEntry
+          onCancel={this.cancelDelete}
+          onDelete={this.delete}
+          itemToDeleteID={itemToDeleteID}
+          visible={modalConfirmDeleteVisible}
+        />
         <div className="col-6">
           <h2 className="text-center">Entries</h2>
           {entriesDOM}
