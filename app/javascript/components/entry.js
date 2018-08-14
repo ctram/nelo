a0fa2bf0af753875d75879ca6b8f6c2b33720474
Chris Tram <chris.tram@gmail.com>
2018-08-14 12:18:42 -0700

    set user's content as markup

--- app/javascript/components/entry.js
+++ app/javascript/components/entry.js
@@ -5,10 +5,20 @@ export default class Entry extends React.Component {
   constructor(props) {
     super(props);
     this.delete = this.delete.bind(this);
+    this.generateContentDOM = this.generateContentDOM.bind(this);
+    this.createMarkup = this.createMarkup.bind(this);
   }
 
   delete() {
     this.props.onDelete(this.props.entry.id);
+  }
+
+  generateContentDOM(content) {
+    return content.replace(/\r\n/g, '<br/>').replace(/ /g, '&nbsp;');
+  }
+
+  createMarkup(content) {
+    return { __html: this.generateContentDOM(content) };
   }
 
   render() {
@@ -19,7 +29,10 @@ export default class Entry extends React.Component {
       <div className="entry d-flex justify-content-between">
         <div className="entry__details">
           <h3>{entryTitle}</h3>
-          <div>{entryContent}</div>
+          <div
+            className="entry__content"
+            dangerouslySetInnerHTML={this.createMarkup(entryContent)}
+          />
         </div>
         <div className="entry__actions">
           <a
