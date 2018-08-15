dc87ce88e7776d29d12887881ac817c84f0483c8
Chris Tram <chris.tram@gmail.com>
2018-08-15 02:24:03 -0700

    parse return carriages and spaces into html code -- escape user written brackets to prevent user manipulation

--- app/javascript/components/entry.js
+++ app/javascript/components/entry.js
@@ -14,7 +14,10 @@ export default class Entry extends React.Component {
   }
 
   generateContentDOM(content) {
-    return content.replace(/\r\n/g, '<br/>').replace(/ /g, '&nbsp;');
+    return content
+      .replace(/<|>/g, '') // TODO: determine way for user to safely use brackets
+      .replace(/\r\n/g, '<br/>')
+      .replace(/ /g, '&nbsp;');
   }
 
   createMarkup(content) {
