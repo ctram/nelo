7dd8299c60350a5ff1dd8866e3c24a03414f26fc
Chris Tram <chris.tram@gmail.com>
2018-08-18 02:38:26 -0700

    adds user and message associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   validates_presence_of :content, :privacy_level, :author_id, :recipient_id
-  
+
+  has_many :user_as_author, class_name: 'User', foreign_key: 'author_id'
+  has_many :user_as_recipient, class_name: 'User', foreign_key: 'recipient_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :entries, dependent: :destroy
+  has_many :messages_as_recipient, class_name: 'Message', foreign_key: 'recipient_id'
+  has_many :messages_as_author, class_name: 'Message', foreign_key: 'author_id'
+
+  def all_messages
+    Message.where('author_id = ? OR recipient_id = ?', current_user.id, current_user.id)
+  end
 end
