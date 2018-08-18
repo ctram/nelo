51f81dbbde0ae5bb2f9a0298189e0a49475a5ce7
Chris Tram <chris.tram@gmail.com>
2018-08-18 11:47:47 -0700

    adds association methods for user to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   validates_presence_of :content, :privacy_level, :author_id, :recipient_id
 
-  has_many :user_as_author, class_name: 'User', foreign_key: 'author_id'
-  has_many :user_as_recipient, class_name: 'User', foreign_key: 'recipient_id'
+  belongs_to :author,     class_name: 'User', foreign_key: 'author_id'
+  belongs_to :recipient,  class_name: 'User', foreign_key: 'recipient_id'
 end
