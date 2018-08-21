bc266e779428dee3ad7849a7b20681d0e8c2924c
Chris Tram <chris.tram@gmail.com>
2018-08-21 00:08:09 -0700

    adds scope for entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,8 @@
 class Entry < ApplicationRecord
   validates_presence_of :content, :title
+
+  scope :privacy_private, -> { where(privacy_level: 'private') }
+  scope :privacy_public, -> { where(privacy_level: 'public') }
   
   belongs_to :user
 end
