8227f68004410005e564b1ecdeafdbe5a6bed403
Chris Tram <chris.tram@gmail.com>
2018-08-20 23:06:18 -0700

    redirect to entries index if signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
-    length = Entry.all.length
-    start_idx = length - 10 > 0 ? length - 10 : 0
-    @entries = Entry.all.slice(start_idx, length)
+    redirect_to "/users/#{current_user.id}/entries" if signed_in?
   end
 end
