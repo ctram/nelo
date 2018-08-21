0baafe9a1edfb4a0d1dd39370858d892df75c453
Chris Tram <chris.tram@gmail.com>
2018-08-21 00:08:43 -0700

    add scopes and validation that recipient and author cannot be the same, for message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,16 @@
 class Message < ApplicationRecord
   validates_presence_of :content, :privacy_level, :author_id, :recipient_id
+  validate :recipient_and_author_cannot_be_the_same
+
+  scope :privacy_public, -> { where(privacy_level: 'public') }
+  scope :privacy_private, -> { where(privacy_level: 'private') }
 
   belongs_to :author,     class_name: 'User', foreign_key: 'author_id'
   belongs_to :recipient,  class_name: 'User', foreign_key: 'recipient_id'
+
+  private
+
+  def recipient_and_author_cannot_be_the_same
+    errors.add(:author_id, 'cannot be the same as recipient_id') if author_id == recipient_id
+  end
 end
