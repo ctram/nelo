54d6bf764cd33f624f5cef914f9019bdad246367
Chris Tram <chris.tram@gmail.com>
2018-08-23 15:21:58 -0700

    adds 'private' badge to entry component

--- app/javascript/components/entry.js
+++ app/javascript/components/entry.js
@@ -10,15 +10,32 @@ export default class Entry extends React.Component {
     const { entry, type } = this.props;
     const { title: entryTitle, content: entryContent } = entry;
     let cssClass = 'entry d-flex ';
+    let badgeDOM = null;
+    let badgeContent = null;
     cssClass +=
       type === 'show-page'
         ? 'flex-column-reverse align-items-start entry--show-page'
         : 'justify-content-between entry--index-page';
 
+    if (entry.privacy_level === 'private') {
+      badgeContent = 'Private';
+    } else if (entry.privacy_level === 'friends') {
+      badgeContent = 'Friends';
+    }
+
+    if (badgeContent) {
+      badgeDOM = (
+        <div className="entry__badges">
+          <span className="badge badge-secondary">{badgeContent}</span>;
+        </div>
+      );
+    }
+
     return (
       <div className={cssClass}>
         <div className="entry__details">
           <h4>{entryTitle}</h4>
+          {badgeDOM}
           <div
             className="entry__content"
             dangerouslySetInnerHTML={MarkupHelpers.createMarkup(entryContent)}
