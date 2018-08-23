6aefbba78e3e11c18ad6d2881dc7452a902b3ae4
Chris Tram <chris.tram@gmail.com>
2018-08-23 15:23:12 -0700

    fix entry association to user as author

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,5 +4,5 @@ class Entry < ApplicationRecord
   scope :privacy_private, -> { where(privacy_level: 'private') }
   scope :privacy_public, -> { where(privacy_level: 'public') }
   
-  belongs_to :user
+  belongs_to :author, class_name: User
 end
