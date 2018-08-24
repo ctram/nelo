50c97c07f26bcb4b9e214792c597776958e1c11c
Chris Tram <chris.tram@gmail.com>
2018-08-24 16:45:36 -0700

    expose message author via entity

--- app/entities/message_entity.rb
+++ app/entities/message_entity.rb
@@ -3,7 +3,7 @@ module API
     class MessageEntity < Grape::Entity
       expose :id
       expose :author, using: API::Entities::UserEntity
-      expose :recipient
+      expose :recipient, using: API::Entities::UserEntity
       expose :content
       expose :updated_at
     end
