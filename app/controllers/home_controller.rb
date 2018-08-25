09b775747a15b3b17cb370a132a1255ce8c9a8f1
Chris Tram <chris.tram@gmail.com>
2018-08-24 17:48:14 -0700

    update home controller for user not logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,16 @@
 class HomeController < ApplicationController
+  skip_before_action :redirect_if_not_logged_in
+  
   def index
-    @entries = Entry.privacy_public.reverse_order
-    @messages = Message.privacy_public.reverse_order
+    if signed_in?
+      @entries = Entry.with_user_entries(current_user).reverse_order
+      @messages = Message.with_user_messages(current_user).reverse_order
+    else
+      @entries = Entries.privacy_public.reverse_order
+      @messages = Messages.privacy_public.reverse_order
+    end
+    
+    @entries = API::Entities::EntryEntity.represent(@entries).as_json
+    @messages = API::Entities::MessageEntity.represent(@messages).as_json
   end
 end
